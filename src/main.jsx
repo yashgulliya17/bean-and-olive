f30b296cdf64e4527345ffb988c406b79bc852bd
Yash Gulliya <yashgulliya@Yashs-MacBook-Pro.local>
2026-06-07 00:31:36 -0400

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App.jsx';
+import { Analytics } from "@vercel/analytics/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <HelmetProvider>
         <App />
+        <Analytics />
       </HelmetProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
